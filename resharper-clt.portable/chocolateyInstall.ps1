4d18ab5726bf3fb630c0bd4fa6ed2b0a98fac2d9
derigel <derigel@mail.ru>
2014-03-22 23:35:48 +0400

    ReSharper CLT 8.2 release

--- resharper-clt.portable/chocolateyInstall.ps1
+++ resharper-clt.portable/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyZipPackage 'resharper-clt.portable' 'http://download.jetbrains.com/resharper/jb-commandline-8.1.23.523.zip'  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿Install-ChocolateyZipPackage 'resharper-clt.portable' 'http://download.jetbrains.com/resharper/jb-commandline-8.2.0.2151.zip'  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
