5da1a52bfb46bb07cf3e671b09b7b73499f5dcab
Justin James <digitaldrummerj@gmail.com>
2014-12-17 10:29:25 -0700

    updated webstorm to 9.0.2

--- WebStorm/chocolateyInstall.ps1
+++ WebStorm/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'WebStorm' # arbitrary name for the package, used in messages
 $installerType = 'EXE' #only one of these two: exe or msi
-$url = 'http://download.jetbrains.com/webstorm/WebStorm-8.0.4.exe' # download url
+$url = 'http://download.jetbrains.com/webstorm/WebStorm-9.0.2.exe' # download url
 $url64 = $url # 64bit URL here or just use the same as $url
 $silentArgs = '/S' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
