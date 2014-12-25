6ff08152cd8aaaf8300c31e59c4c756af69a4b5d
derigel <derigel@mail.ru>
2014-12-25 20:22:55 +0300

    ReSharper 9.0.1

--- resharper-platform/chocolateyInstall.ps1
+++ resharper-platform/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 $packageName = 'resharper-platform'
 
-$url = 'http://download.jetbrains.com/resharper/ReSharperAndToolsPacked01.exe'
+$url = 'http://download.jetbrains.com/resharper/ReSharperAndToolsPacked01Update1.exe'
 
 try {
 
   $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
   $packagePath = $(Split-Path -parent $scriptPath)
-  $installPath = Join-Path $packagePath 'ReSharperAndToolsPacked01.exe'
+  $installPath = Join-Path $packagePath 'ReSharperAndToolsPacked01Update1.exe'
 
   Get-ChocolateyWebFile $packageName $installPath $url
   
