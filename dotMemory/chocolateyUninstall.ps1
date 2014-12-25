d04798d8375d45f4b59fd44774f97367b4f2126c
derigel <derigel@mail.ru>
2014-12-25 20:34:16 +0300

    dotMemory 4.2.1

--- dotMemory/chocolateyUninstall.ps1
+++ dotMemory/chocolateyUninstall.ps1
@@ -1,12 +1,12 @@
 $platformPackageName = 'resharper-platform'
-$platformPackageVersion = '1.0'
+$platformPackageVersion = '1.0.1'
 $packageName = 'dotMemory'
 
 try {
   $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
   $commonPath = $(Split-Path -parent $(Split-Path -parent $scriptPath))
 
-  $installPath = Join-Path  (Join-Path $commonPath $platformPackageName'.'$platformPackageVersion) 'ReSharperAndToolsPacked01.exe'
+  $installPath = Join-Path  (Join-Path $commonPath $platformPackageName'.'$platformPackageVersion) 'ReSharperAndToolsPacked01Update1.exe'
 
   Uninstall-ChocolateyPackage packageName 'exe' '/SpecificProductNames=dotMemory /HostsToRemove=dotMemory01;ReSharperPlatformVs10;ReSharperPlatformVs11;ReSharperPlatformVs12;ReSharperPlatformVs14 /Hive=* /ReSharper9PlusMsi=True' $installPath
 
